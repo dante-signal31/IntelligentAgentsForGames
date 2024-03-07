14395f523cf2c91070b4d3295aea67bb9295717c
dante-signal31 <dante.signal31@gmail.com>
2024-03-07 20:24:31 +0100

    Commented code removed.

--- Assets/Editor/DrawPursuitCones.cs
+++ Assets/Editor/DrawPursuitCones.cs
@@ -17,9 +17,7 @@ namespace Editor
         
             EditorGUI.BeginChangeCheck();
             (float newAheadSemiConeDegrees, float _) = DrawAheadCone(pursuer);
-            // float newAheadSemiConeRadians = DrawAheadCone(pursuer);
             (float newComingToUsSemiConeDegrees, float _) = DrawComingToUsCone(pursuer);
-            // float newComingToUsSemiConeRadians = DrawComingToUsCone(pursuer);
             if (EditorGUI.EndChangeCheck())
             {
                 Undo.RecordObject(pursuer, "Changed ahead semicone degrees.");
@@ -63,48 +61,14 @@ namespace Editor
         }
 
         private static (float, float) DrawAheadCone(PursuitSteeringBehavior pursuer)
-        // private static float DrawAheadCone(PursuitSteeringBehavior pursuer)
         {
-            // Handles.color = new Color(0f, 0f, 1f, 0.1f);
-            // Vector3 pursuerPosition = pursuer.transform.position;
-            // float aheadSemiConeDegrees = Mathf.Rad2Deg * pursuer.AheadSemiConeRadians;
-            // Vector3 forward = pursuer.transform.up;
-            // // Rotate in local space.
-            // // I don't know why rotated vector is only 0.5 length so I have to multiply by two to get a 1 unit length vector.
-            // Vector3 rotatedVector = Quaternion.AngleAxis(aheadSemiConeDegrees, Vector3.forward) * Vector3.up * 2;
-            // // Handle is placed in world space so rotated vector must be taken back to world space.
-            // Vector3 aheadSemiConePosition = Handles.PositionHandle(pursuer.transform.TransformPoint(rotatedVector), 
-            //     Quaternion.identity);
-            // Handles.DrawSolidArc(pursuerPosition, Vector3.forward, 
-            //     forward, aheadSemiConeDegrees, 1);
-            // Handles.DrawSolidArc(pursuerPosition, Vector3.forward,
-            //     forward, -1 * aheadSemiConeDegrees, 1);
-            // // Again, we rotate in local space, so aheadSemiConePosition is taken back to local space.
-            // float newAheadSemiConeRadians =  Mathf.Deg2Rad * Vector2.Angle(Vector3.up, 
-            //     pursuer.transform.InverseTransformPoint(aheadSemiConePosition));
-            // return newAheadSemiConeRadians;
             return RangeCone(pursuer.transform, pursuer.AheadSemiConeDegrees,
                 Vector3.up, Vector3.forward, 
                 new Color(0f, 0f, 1f, 0.1f), 1.0f, false);
         }
     
         private static (float, float) DrawComingToUsCone(PursuitSteeringBehavior pursuer)
-        // private static float DrawComingToUsCone(PursuitSteeringBehavior pursuer)
         {
-            // Handles.color = new Color(1, 0.92f, 0.016f, 0.3f);
-            // Vector3 pursuerPosition = pursuer.transform.position;
-            // float comingToUsConeDegrees = Mathf.Rad2Deg * pursuer.ComingToUsSemiConeRadians;
-            // Vector3 forward = pursuer.transform.up;
-            // Vector3 rotatedVector = Quaternion.AngleAxis(comingToUsConeDegrees, Vector3.forward) * Vector3.up * 2;
-            // Vector3 comingToUsSemiConePosition = Handles.PositionHandle(pursuer.transform.TransformPoint(rotatedVector),
-            //     Quaternion.identity);
-            // Handles.DrawSolidArc(pursuerPosition, Vector3.forward, 
-            //     -forward, (180 - comingToUsConeDegrees), 1);
-            // Handles.DrawSolidArc(pursuerPosition, Vector3.forward, 
-            //     -forward, -1 * (180 - comingToUsConeDegrees), 1);
-            // float newComingToUsSemiConeRadians = Mathf.Deg2Rad * Vector2.Angle(Vector3.up, 
-            //     pursuer.transform.InverseTransformPoint(comingToUsSemiConePosition));
-            // return newComingToUsSemiConeRadians;
             return RangeCone(pursuer.transform, pursuer.ComingToUsSemiConeDegrees,
                 -Vector3.up, Vector3.forward, 
                 new Color(1, 0.92f, 0.016f, 0.3f), 1.0f, false);
