094545f459bf0c05052722082ac4e83bb79d8dba
dante-signal31 <dante.signal31@gmail.com>
2024-03-16 23:53:09 +0100

    Added and interactive handle to define sectors.

--- Assets/Editor/Tools/InteractiveRanges.cs
+++ Assets/Editor/Tools/InteractiveRanges.cs
@@ -50,5 +50,64 @@ namespace Editor.Tools
                 parentTransform.InverseTransformPoint(newHandlePosition));
             return (newSemiConeDegrees, range);
         }
+
+        /// <summary>
+        /// Show a range sector with handle to set its angular width and its range.
+        ///
+        /// Whereas a cone laterals go from center to its range, a sector laterals go
+        /// from a minimum range to its range. 
+        /// </summary>
+        /// <param name="parentTransform">Transform of the GameObject this cone is attached to.</param>
+        /// <param name="semiConeDegrees">Half angular width in degrees for this cone.</param>
+        /// <param name="localConeForward">Forward direction for this cone in local space.</param>
+        /// <param name="coneNormal">Normal vector for this cone plane.</param>
+        /// <param name="coneColor">Color for this cone.</param>
+        /// <param name="range">Length of this cone.</param>
+        /// <param name="minimumRange">Minimum length of this cone.</param>
+        /// <param name="takeNewRange">Return the updated range after moving handle</param>
+        /// <returns>Tuple of (new semiConeDegrees, new range, new minimumRange)</returns>
+        public static (float, float, float) RangeSector(Transform parentTransform,
+            float semiConeDegrees,
+            Vector3 localConeForward, Vector3 coneNormal, Color coneColor,
+            float range = 0.5f, float minimumRange = 0.2f, bool takeNewRange = false)
+        {
+            Handles.color = coneColor;
+            Vector3 position = parentTransform.position;
+            Vector3 globalForward = parentTransform.TransformDirection(localConeForward);
+            // Rotate in local space.
+            // I don't know why rotated vector is only 0.5 length so I have to multiply by two to get a 1 unit length vector.
+            Vector3 rotatedVector = Quaternion.AngleAxis(semiConeDegrees, coneNormal) * localConeForward;
+            // Handle is placed in world space so rotated vector must be taken back to world space.
+            Vector3 handlePosition = parentTransform.TransformPoint(rotatedVector * range);
+            Vector3 newHandlePosition = Handles.PositionHandle(
+                handlePosition, 
+                Quaternion.identity);
+            Vector3 minimumRangeHandlePosition = parentTransform.TransformPoint(rotatedVector * minimumRange);
+            Vector3 newMinimumRangeHandlePosition = Handles.PositionHandle(
+                minimumRangeHandlePosition,
+                Quaternion.identity);
+            range = takeNewRange ? Vector3.Distance(position, newHandlePosition) : range;
+            minimumRange = takeNewRange ? Vector3.Distance(position, newMinimumRangeHandlePosition) : minimumRange;
+            // Outer arc.
+            Handles.DrawWireArc(position, coneNormal, globalForward, semiConeDegrees, range);
+            Handles.DrawWireArc(position, coneNormal,
+                globalForward, -1 * semiConeDegrees, range);
+            // Inner arc.
+            Handles.DrawWireArc(position, coneNormal, globalForward, semiConeDegrees, minimumRange);
+            Handles.DrawWireArc(position, coneNormal,
+                globalForward, -1 * semiConeDegrees, minimumRange);
+            // Lateral lines.
+            Handles.DrawLine(minimumRangeHandlePosition, handlePosition);
+            Vector3 lateralLineVector = handlePosition - minimumRangeHandlePosition;
+            Vector3 localLateralLineVector = parentTransform.InverseTransformDirection(lateralLineVector);
+            Vector3 rotatedLateralLineVector = Quaternion.AngleAxis(-2 * semiConeDegrees, coneNormal) * localLateralLineVector;
+            Vector3 rotatedLateralLineVectorNormalized = rotatedLateralLineVector.normalized;
+            Handles.DrawLine(rotatedLateralLineVectorNormalized * minimumRange, 
+                rotatedLateralLineVectorNormalized * range);
+            // Again, we rotate in local space, so aheadSemiConePosition is taken back to local space.
+            float newSemiConeDegrees =  Vector2.Angle(localConeForward, 
+                parentTransform.InverseTransformPoint(newHandlePosition));
+            return (newSemiConeDegrees, range, minimumRange);
+        }
     }
 }
