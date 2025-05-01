737e16c682b8c087b3ef2e57921f64953496d916
dante-signal31 <dante.signal31@gmail.com>
2025-05-02 00:18:46 +0200

    Added configuration handle to FollowOffsetBehavior.

---
+++ Assets/Editor/Handles/DrawFollowOffsetMarker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Editor.Tools;
+using SteeringBehaviors;
+
+namespace Editor
+{
+    [CustomEditor(typeof(OffsetFollowBehavior))]
+    public class DrawFollowOffsetMarker : UnityEditor.Editor
+    {
+        private void OnSceneGUI()
+        {
+            var follower = (OffsetFollowBehavior)target;
+            
+            if (follower.Target == null) return;
+        
+            EditorGUI.BeginChangeCheck();
+            
+            // Draw handle to place offset marker visually.
+            Handles.color = Color.red;
+            Vector2 newMarkerPosition = Handles.PositionHandle(
+                follower.Target.transform.TransformPoint(follower.offsetFromTarget),
+                Quaternion.identity);
+            
+            // Draw lines to see visual aids in the scene tab.
+            Handles.color = Color.green;
+            Handles.DrawLine(follower.transform.position, newMarkerPosition);
+            Handles.DrawWireDisc(newMarkerPosition, Vector3.forward, 0.1f);
+            Handles.DrawLine(newMarkerPosition, follower.Target.transform.position);
+            
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(follower, "Changed follow offset.");
+                follower.offsetFromTarget = 
+                    follower.Target.transform.InverseTransformPoint(newMarkerPosition);
+            }
+        }
+    }
+}
