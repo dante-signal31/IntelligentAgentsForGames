8d115c0ab8735d09f644f49edb2dda0f99e1de4f
dante-signal31 <dante.signal31@gmail.com>
2025-10-24 22:41:41 +0200

    Now measure tape shows read only fields with global position.

--- Assets/Editor/Handles/DrawMeasureTape.cs
+++ Assets/Editor/Handles/DrawMeasureTape.cs
@@ -1,6 +1,9 @@
-﻿using Tools;
+﻿using Groups;
+using Tools;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 [CustomEditor(typeof(MeasureTape))]
@@ -8,6 +11,91 @@ using UnityEngine;
 // drawn if this script is under Editor namespace.
 public class DrawMeasureTape : UnityEditor.Editor
 {
+    private SerializedProperty _localPositionA;
+    private SerializedProperty _localPositionB;
+    private SerializedProperty _color;
+    private SerializedProperty _thickness;
+    private SerializedProperty _endWidth;
+    private SerializedProperty _endAlignment;
+    private SerializedProperty _textSize;   
+    private SerializedProperty _textDistance;
+    
+    private void OnEnable()
+    {
+        _localPositionA = serializedObject.FindProperty("localPositionA");
+        _localPositionB = serializedObject.FindProperty("localPositionB");
+        _color = serializedObject.FindProperty("color");
+        _thickness = serializedObject.FindProperty("thickness");
+        _endWidth = serializedObject.FindProperty("endWidth");
+        _endAlignment = serializedObject.FindProperty("endAlignment");
+        _textSize = serializedObject.FindProperty("textSize");
+        _textDistance = serializedObject.FindProperty("textDistance");
+    }
+    
+    public override VisualElement CreateInspectorGUI()
+    {
+        
+        VisualElement root = new()
+        {
+            style =
+            {
+                // Add elements in descending column order.
+                flexDirection = new StyleEnum<FlexDirection>(FlexDirection.Column)
+            }
+        };
+
+        MeasureTape tape = (MeasureTape)serializedObject.targetObject;
+        
+        // Just add those properties we want to be shown in its default view.
+        var localPositionAField = new PropertyField(_localPositionA);
+        root.Add(localPositionAField);
+        var localPositionBField = new PropertyField(_localPositionB);
+        root.Add(localPositionBField);
+        
+        // Create the panel where we will show global position fields.
+        VisualElement globalPositionFields = new VisualElement();
+        root.Add(globalPositionFields);
+        
+        // Populate the panel with global positions fields.
+        UpdateGlobalPositionFields(globalPositionFields, tape);
+        
+        // When changed local position fields, or their handles, update global position
+        // fields.
+        localPositionAField.RegisterValueChangeCallback(_ => 
+            UpdateGlobalPositionFields(globalPositionFields, tape));
+        localPositionBField.RegisterValueChangeCallback(_ => 
+            UpdateGlobalPositionFields(globalPositionFields, tape));
+        
+        // Add every other property we want to show in its default view.
+        root.Add(new PropertyField(_color));
+        root.Add(new PropertyField(_thickness));
+        root.Add(new PropertyField(_endWidth));
+        root.Add(new PropertyField(_endAlignment));
+        root.Add(new PropertyField(_textSize));
+        root.Add(new PropertyField(_textDistance));
+        
+        return root;
+    }
+
+    private void UpdateGlobalPositionFields(VisualElement panel, MeasureTape tape)
+    {
+        panel.Clear();
+        
+        panel.style.flexDirection = 
+            new StyleEnum<FlexDirection>(FlexDirection.Column);
+        
+        var positionAField = new Vector3Field("Global Position A");
+        positionAField.value = tape.PositionA;
+        positionAField.SetEnabled(false);
+        panel.Add(positionAField);
+        
+        var positionBField = new Vector3Field("Global Position B");
+        positionBField.value = tape.PositionB;
+        positionBField.SetEnabled(false);
+        panel.Add(positionBField);
+    }
+
+    // Use DrawGizmo to draw the tape even when the tape is not selected.
     [DrawGizmo(GizmoType.Selected | GizmoType.NonSelected)]
     static void DrawTapeGizmos(MeasureTape tape, GizmoType gizmoType)
     {
