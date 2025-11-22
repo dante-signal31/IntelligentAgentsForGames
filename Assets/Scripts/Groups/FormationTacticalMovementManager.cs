6e64c0f8698976935a34d470be917fd9702ae06b
dante-signal31 <dante.signal31@gmail.com>
2025-11-23 00:59:34 +0100

    First untested version of FormationTacticalMovementManager.

---
+++ Assets/Scripts/Groups/FormationTacticalMovementManager.cs
@@ -0,0 +1,276 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Levels;
+using PropertyAttribute;
+using SteeringBehaviors;
+using Tools;
+using UnityEngine;
+
+namespace Groups
+{
+/// <summary>
+/// Manages the tactical movement of agents within a formation. Makes formation members
+/// take cover when a hiding point becomes available near their path.
+/// </summary>
+public class FormationTacticalMovementManager : MonoBehaviour, IGizmos
+{
+    [Header("CONFIGURATION:")] 
+    // _maximumDistanceFromUshers should be lower than UsherWaiterFormation
+    // MaximumLaggingBehindDistance, otherwise agents could stay behind cover so long
+    // that formation could get stuck with no further advance.
+    [Tooltip("Maximum separation from ushers allowed for agents to take cover.")]
+    [SerializeField] private float maximumDistanceFromUshers = 3.0f;
+    [Tooltip("Time to let pass between checks for cover.")]
+    [SerializeField] private float detectionCoolDown = 0.5f;
+    
+    [Header("WIRING:")]
+    [InterfaceCompliant(typeof(ITargeter))]
+    [SerializeField] private SteeringBehavior iTargeterBehavior;
+    [InterfaceCompliant(typeof(IFormation))]
+    [SerializeField] private MonoBehaviour iFormationMembers;
+    [InterfaceCompliant(typeof(IFormation))]
+    [SerializeField] private MonoBehaviour iFormationUshers;
+    [SerializeField] private FormationPattern formationPattern;
+    // Make sure to configure the HidingPointsDetector node with a SeparationFromObstacles
+    // value higher than the range of the WhiskerSensors of the agents in the formation.
+    // Otherwise, agents sensor will touch covers when approaching hiding points, which
+    // will trigger an avoidance movement.
+    [SerializeField] private HidingPointsDetector hidingPointsDetector;
+    
+    [Header("DEBUG:")]
+    [SerializeField] private bool showGizmos;
+    [SerializeField] private Color gizmosColor;
+
+    public bool ShowGizmos
+    {
+        get => showGizmos;
+        set => showGizmos = value;
+    }
+
+    public Color GizmosColor
+    {
+        get => gizmosColor;
+        set => gizmosColor = value;
+    }
+    
+    private ITargeter _targeter;
+    private IFormation _formationMembers;
+    private IFormation _formationUshers;
+    private System.Timers.Timer _detectionCooldownTimer;
+    private bool _waitingForDetectionCooldownTimeout;
+    private Vector2[] _availableHidingPoints;
+    private GameObject[] _formationNodesHidingPointTargets;
+    private bool _formationNodesHidingPointTargetsInitialized;
+    private bool _currentTargetChanged;
+    private GameObject _currentTarget;
+    
+    private GameObject CurrentTarget
+    {
+        get => _currentTarget;
+        set
+        {
+            if (_currentTarget == value) return;
+            _currentTarget = value;
+            hidingPointsDetector.Threat = value;
+            _currentTargetChanged = true;
+        }
+    }
+
+    private void OnEnable()
+    {
+        SetTimer();
+    }
+
+    private void OnDisable()
+    {
+        _detectionCooldownTimer.Elapsed -= OnTimerTimeout;
+    }
+    
+    private void SetTimer()
+    {
+        _detectionCooldownTimer = 
+            new System.Timers.Timer(detectionCoolDown * 1000);
+        _detectionCooldownTimer.AutoReset = false;
+        _detectionCooldownTimer.Elapsed += OnTimerTimeout;
+    }
+    
+    private void StartDetectionCooldownTimer()
+    {
+        _waitingForDetectionCooldownTimeout = true;
+        _detectionCooldownTimer.Stop();
+        _detectionCooldownTimer.Start();
+    }
+    
+    private void StopDetectionCooldownTimer()
+    {
+        _waitingForDetectionCooldownTimeout = false;
+        _detectionCooldownTimer.Stop();
+    }
+    
+    private void OnTimerTimeout(object sender, System.Timers.ElapsedEventArgs e)
+    {
+        _waitingForDetectionCooldownTimeout = false;
+    }
+    
+    /// <summary>
+    /// Redirects a specific agent to a designated hiding point. Updates the agent's
+    /// target position and assigns the provided hiding point as its new target for
+    /// tactical movement.
+    /// </summary>
+    /// <param name="agentIndex">
+    /// The index of the agent within the formation that is being redirected to the
+    /// hiding point.
+    /// </param>
+    /// <param name="hidingPoint">
+    /// The global position of the hiding point to which the agent will be redirected.
+    /// </param>
+    private void RedirectAgentToHidingPoint(int agentIndex, Vector2 hidingPoint)
+    {
+        ITargeter agentTargeter = 
+            _formationMembers.Members[agentIndex].GetComponentInChildren<ITargeter>();
+        _formationNodesHidingPointTargets[agentIndex].transform.position = hidingPoint;
+        agentTargeter.Target = _formationNodesHidingPointTargets[agentIndex];
+    }
+
+    /// <summary>
+    /// Redirects the specified agents to target the formation ushers. Each agent is
+    /// assigned to the corresponding usher in the formation based on their index. This
+    /// method ensures agents without suitable hiding points are redirected towards the
+    /// ushers.
+    /// </summary>
+    /// <param name="agentsToRedirect">
+    /// A set of agent indices representing the agents to be redirected to the formation
+    /// ushers.
+    /// </param>
+    private void RedirectAgentsToUshers(HashSet<int> agentsToRedirect)
+    {
+        foreach (int agentIndex in agentsToRedirect)
+        {
+            ITargeter agentTargeter = 
+                _formationMembers.Members[agentIndex].GetComponentInChildren<ITargeter>();
+            if (agentTargeter != null)
+            {
+                agentTargeter.Target = _formationUshers.Members[agentIndex];
+            }
+        }
+    }
+
+    private void Start()
+    {
+        _targeter = (ITargeter) iTargeterBehavior;
+        _formationMembers = (IFormation) iFormationMembers;
+        _formationUshers = (IFormation) iFormationUshers;
+        hidingPointsDetector.Threat = _targeter.Target;
+        Courtyard currentLevel = FindAnyObjectByType<Courtyard>();
+        hidingPointsDetector.ObstaclesPositions = currentLevel.ObstaclePositions;
+        _currentTarget = _targeter.Target;
+    }
+    
+    /// <summary>
+    /// Assigns suitable hiding points to agents within the defined maximum distance
+    /// from the positions of the ushers in the formation. For each agent, the nearest
+    /// hiding point to the current target is selected if available. If a suitable hiding
+    /// point is found, the agent is redirected to it.
+    /// </summary>
+    /// <returns>
+    /// A set of agent indices that have been successfully assigned suitable hiding
+    /// points.
+    /// </returns>
+    private HashSet<int> AssignSuitableHidingPointsToAgents()
+    {
+        int agentIndex = 0;
+        HashSet<int> agentsWithSuitableHidingPoints = new();
+        foreach (Vector2 usherLocalPosition in _formationUshers.MemberPositions)
+        {
+            // Find the hiding points inside the maximum distance from the usher's
+            // position.
+            Vector2 usherGlobalPosition = 
+                transform.TransformPoint(usherLocalPosition);
+            Vector2[] nearbyHidingPoints = _availableHidingPoints
+                .Where(hidingPoint => 
+                    Vector2.Distance(hidingPoint, usherGlobalPosition) <= 
+                    maximumDistanceFromUshers)
+                .ToArray();
+
+            // Find the nearest hiding point to the current usher's position.
+            Vector2 nearestHidingPoint = Vector2.zero;
+            if (nearbyHidingPoints.Length > 0)
+            {
+                nearestHidingPoint = nearbyHidingPoints
+                    .OrderBy(hidingPoint => 
+                        Vector2.Distance(hidingPoint, usherGlobalPosition))
+                    .First();
+            }
+            
+            // Assign the hiding point if any.
+            if (nearestHidingPoint != Vector2.zero)
+            {
+                RedirectAgentToHidingPoint(agentIndex, nearestHidingPoint);
+                agentsWithSuitableHidingPoints.Add(agentIndex);
+            }
+            
+            agentIndex++;
+        }
+        return agentsWithSuitableHidingPoints;
+    }
+
+    /// <summary>
+    /// Initializes the hiding point targets for each node in the formation. Creates a new
+    /// GameObject for each member in the formation to serve as the initial hiding point
+    /// target placeholder. This method is called once when the formation nodes are not
+    /// yet initialized.
+    /// </summary>
+    private void InitNodesHidingPointTargets()
+    {
+        _formationNodesHidingPointTargets = new GameObject[_formationMembers.MemberPositions.Count];
+        for (int i = 0; i < _formationNodesHidingPointTargets.Length; i++)
+        {
+            _formationNodesHidingPointTargets[i] = new GameObject();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_targeter.Target != CurrentTarget) CurrentTarget = _targeter.Target;
+
+        if (!_formationNodesHidingPointTargetsInitialized)
+        {
+            // This cannot be done in Start() because formationMembers.MemberPosition
+            // is not yet ready.
+            InitNodesHidingPointTargets();
+            _formationNodesHidingPointTargetsInitialized = true;            
+        }
+        
+        // Our calculations are a heavy load. So, we'd better not do it every frame. 
+        if (_waitingForDetectionCooldownTimeout && !_currentTargetChanged) return;
+        _currentTargetChanged = false;
+        
+        _availableHidingPoints = hidingPointsDetector.HidingPoints.ToArray();
+        HashSet<int> agentsWithHidingPoints = AssignSuitableHidingPointsToAgents();
+        HashSet<int> agentsWithNoHidingPoints = 
+            Enumerable.Range(0, _formationMembers.Members.Count)
+                .Except(agentsWithHidingPoints)
+                .ToHashSet();
+        RedirectAgentsToUshers(agentsWithNoHidingPoints);
+        
+        StartDetectionCooldownTimer();
+    }
+    
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (!showGizmos || formationPattern == null) return;
+        Gizmos.color = gizmosColor;
+        
+        // Draw range to find hiding positions for every 
+        foreach (Vector2 usherLocalPosition in formationPattern.Positions.Offsets)
+        {
+            Gizmos.DrawWireSphere(
+                transform.TransformPoint(usherLocalPosition),
+                maximumDistanceFromUshers);
+        }
+    }
+#endif
+}
+}
+
